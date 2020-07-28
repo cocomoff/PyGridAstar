c8a0df5485336378d3694b6870c66a7b050a0376
Keisuke Otaki <keisuke.otaki.jp@gmail.com>
2020-07-28 10:33:13 +0900

    update visualizer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import networkx as nx
 import networkx.algorithms as nxa
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 from itertools import product, count
 from heapq import heappush, heappop
 
@@ -48,7 +49,7 @@ def astar_path(G, source, target, heuristic=None, weight='weight', W=1):
                 path.append(node)
                 node = explored[node]
             path.reverse()
-            return path, c, explored
+            return path, c, enqueued, explored
 
         if curnode in explored:
             if explored[curnode] is None:
@@ -72,8 +73,6 @@ def astar_path(G, source, target, heuristic=None, weight='weight', W=1):
             enqueued[neighbor] = ncost, h
             push(queue, (ncost + h, next(c), neighbor, ncost, curnode))
 
-    raise nx.NetworkXNoPath("Node %s not reachable from %s" % (target, source))
-
 if __name__ == '__main__':
     # s = (3, 3)
     # g = (9, 12)
@@ -87,22 +86,34 @@ if __name__ == '__main__':
         return abs(s1[0] - s2[0]) + abs(s1[1] - s2[1])
 
     for W in [0, 1, 2, 3]:
-        apath, c, exp = astar_path(G, s, g, heuristic=h, W=W)
+        apath, c, enq, exp = astar_path(G, s, g, heuristic=h, W=W)
         exp_nodes = list(exp.keys())
+        enq_nodes = list(enq.keys())
 
-        fig = plt.figure()
-        ax = fig.gca()
-        nx.draw(G, pos, ax=ax, node_color='k', node_size=20)
+        segs = []
         for i in range(len(apath) - 1):
             pi = apath[i]
             pj = apath[i + 1]
             posI = pos[pi]
             posJ = pos[pj]
-            ax.plot([posI[0], posJ[0]], [posI[1], posJ[1]], "b-")
+            segs.append([(posI[0], posI[1]), (posJ[0], posJ[1])])
+        lc = LineCollection(segs, color='k', lw=2.5, alpha=0.8)
+
+        # plot
+        fig = plt.figure()
+        ax = fig.gca()
+        nx.draw(G, pos, ax=ax, node_color='k', node_size=20)
+
+        # OPEN
+        nx.draw_networkx_nodes(G, pos, ax=ax, node_color='r', alpha=0.4, node_size=30, nodelist=enq_nodes)
+        # CLOSE
         nx.draw_networkx_nodes(G, pos, ax=ax, node_color='r', alpha=0.8, node_size=30, nodelist=exp_nodes)
-        nx.draw_networkx_nodes(G, pos, ax=ax, node_color='b', alpha=0.8, node_size=30, nodelist=[s])
-        nx.draw_networkx_nodes(G, pos, ax=ax, node_color='m', alpha=0.8, node_size=30, nodelist=[g])
-        ax.set_title(c)
+        # Start/Goal
+        ax.plot([pos[s][0]], [pos[s][1]], color='b', marker='s')
+        ax.plot([pos[g][0]], [pos[g][1]], color='m', marker='s')
+        # Path
+        ax.add_collection(lc)
+        ax.set_title("{}/{}".format(len(exp_nodes), len(enq_nodes)))
         plt.gca().invert_yaxis()
         plt.tight_layout()
         plt.savefig("output/output_W{}.png".format(W), dpi=150)
